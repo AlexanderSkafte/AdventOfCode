8368163a1a83bbbc33a5f5bc17754f940b66f445
Alexander Skafte <alexanderskafte@protonmail.com>
2018-12-02 15:51:27 +0100

    Add function to measure execution time

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type aocFn func() string
@@ -17,9 +18,21 @@ func main() {
 		"      Advent of Code 2018   \n" +
 		"   °~.~°~.~°~.~°~.~°~.~°~.~°\n")
 	for i := 0; i < len(aocs); i += 2 {
-		fmt.Printf("Dec %d:\n", i+1)
-		fmt.Printf("  a) %s\n", aocs[i]())
-		fmt.Printf("  b) %s\n", aocs[i+1]())
+		fmt.Printf("Dec %d:\n", i/2+1)
+
+		a, t := measure(aocs[i])
+		fmt.Printf(" a) [%8s] %s\n", t, a.(string))
+
+		b, t := measure(aocs[i+1])
+		fmt.Printf(" b) [%8s] %s\n", t, b.(string))
+
 		fmt.Println()
 	}
+}
+
+func measure(fn aocFn) (interface{}, time.Duration) {
+	start := time.Now()
+	result := fn()
+	elapsed := time.Since(start)
+	return result, elapsed.Round(time.Millisecond / 10)
 }
