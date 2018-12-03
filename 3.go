263d5fceb5473676009bdc5deccd5be7e17699c7
Alexander Skafte <alexanderskafte@protonmail.com>
2018-12-03 17:30:19 +0100

    Finish 3b

--- 3.go
+++ 3.go
@@ -4,6 +4,40 @@ import (
 	"fmt"
 )
 
+func aoc3b(lines []string) string {
+
+	claims := make([]Claim, len(lines))
+	for i := range lines {
+		claims[i] = parseClaim(lines[i])
+	}
+
+	type XY struct{ x, y int }
+	nbrClaims := map[XY]int{} // Number of claims on a position (x,y)
+
+	for _, c := range claims {
+		for i := c.X; i < c.X+c.W; i++ {
+			for j := c.Y; j < c.Y+c.H; j++ {
+				nbrClaims[XY{i, j}]++
+			}
+		}
+	}
+
+	for _, c := range claims {
+		me := true
+		for i := c.X; i < c.X+c.W; i++ {
+			for j := c.Y; j < c.Y+c.H; j++ {
+				if nbrClaims[XY{i, j}] != 1 {
+					me = false
+				}
+			}
+		}
+		if me == true {
+			return fmt.Sprint(c.ID)
+		}
+	}
+	return "No solution."
+}
+
 func aoc3a(lines []string) string {
 
 	claims := make([]Claim, len(lines))
@@ -12,7 +46,6 @@ func aoc3a(lines []string) string {
 	}
 
 	type XY struct{ x, y int }
-
 	nbrClaims := map[XY]int{} // Number of claims on a position (x,y)
 
 	for _, c := range claims {
@@ -30,8 +63,4 @@ func aoc3a(lines []string) string {
 		}
 	}
 	return fmt.Sprint(count)
-}
-
-func aoc3b(lines []string) string {
-	return "not done"
 }
